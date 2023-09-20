765d53f764a97784817bb70c08c3ae6f7530dcd2
Mateus Queiroz <mateusqueirozdev@gmail.com>
2023-09-20 09:19:13 -0300

    chore: add jest and react testing library

---
+++ jest.config.js
@@ -0,0 +1,17 @@
+const config = {
+  testEnvironment: 'jsdom',
+  testPathIgnorePatterns: ['/node_modules/', '/.next/'],
+  collectCoverage: true,
+  collectCoverageFrom: ['src/**/*.ts(x)?'],
+  setupFilesAfterEnv: ['<rootDir>/.jest/setup.ts'],
+  modulePaths: ['<rootDir>/src/'],
+  transform: {
+    '^.+\\.(js|jsx|ts|tsx)$': ['babel-jest', { presets: ['next/babel'] }]
+  },
+  clearMocks: true,
+  coverageDirectory: 'coverage',
+  coveragePathIgnorePatterns: ['/node_modules/'],
+  maxWorkers: '50%'
+}
+
+module.exports = config
